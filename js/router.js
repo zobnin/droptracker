a3fcfb839305a36723b8cefe5ba04f647a70dbff
Evgeny Zobnin <zobnin@gmail.com>
2016-06-09 15:12:01 +0300

    Cleanup

--- js/router.js
+++ js/router.js
@@ -30,7 +30,7 @@ $(document).on('click', '.img-content-photos-photo', function(e) {
   .show()
 })
 
-// photos-modal
+// screenshots-modal
 $(document).on('click', '.img-content-screenshots-screenshot', function(e) {
   e.preventDefault()
   var url = $(this).attr('src')
@@ -45,25 +45,25 @@ function navigateSection(slug) {
   clearContentContainer()
   switch (slug) {
     case 'logs':
-      $('#content').append('<h2>Logs for device #' + window.DropTracker.STATE.activeDevice +'</h2><div id="content-logs"><img src="img/ajax-loader.gif"></div>')
+      $('#content').append('<div id="content-logs"><img src="img/ajax-loader.gif"></div>')
       getLogs(window.DropTracker.STATE.activeDevice)
       break
     case 'screenshots':
-      $('#content').append('<h2>Screenshots for device #' + window.DropTracker.STATE.activeDevice +'</h2><div id="content-screenshots"><img src="img/ajax-loader.gif"></div>')
+      $('#content').append('<div id="content-screenshots"><img src="img/ajax-loader.gif"></div>')
       getScreenshots(window.DropTracker.STATE.activeDevice)
       break
     case 'photos':
-      $('#content').append('<h2>Photos for device #' + window.DropTracker.STATE.activeDevice +'</h2><div id="content-photos"><img src="img/ajax-loader.gif"></div>')
+      $('#content').append('<div id="content-photos"><img src="img/ajax-loader.gif"></div>')
       getPhotos(window.DropTracker.STATE.activeDevice)
       break
     case 'shell':
-      $('#content').append('<h2>Shell for device #' + window.DropTracker.STATE.activeDevice +'</h2><div id="content-shell"></div>')
+      $('#content').append('<div id="content-shell"></div>')
       // $('#content-shell').append('<div id="shell-input"></div><div id="shell-output"></div>')
       attachShellForm()
       break
     case 'map':
     default:
-      $('#content').append('<h2>Map for device #' + window.DropTracker.STATE.activeDevice +'</h2><div id="content-map"><img src="img/ajax-loader.gif"></div>')
+      $('#content').append('<div id="content-map"><img src="img/ajax-loader.gif"></div>')
       getLastLocation(window.DropTracker.STATE.activeDevice)
   }
 }
